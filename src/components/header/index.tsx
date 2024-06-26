aafafa039b7be9571df11c4bc13f44b0f0fb06d9
Luiz Miller Teixeira <lmillert85@hotmail.com>
2024-06-25 22:05:51 -0300

    havas

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,32 +3,73 @@ import LogoutIcon from '@mui/icons-material/Logout';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import * as S from './style';
 import LogoReanimate from '../../../public/logoreanimatenew.png';
+import logoartmaker4 from '../../../public/logoartmaker4.png'
+import { Divider } from '@mui/material';
 
 const Header = () => {
 	return (
 		<S.HeaderContainer>
-			<div id="control-header-buttons">
+			{/* <div id="control-header-buttons">
 				<HomeIcon className="iconColorOrange" fontSize="large" />
 				<LogoutIcon className="iconColorOrange" fontSize="large" />
-			</div>
-
-			<figure id="reanimate-header-logo">
-				<img
+			</div> */}
+			<img src={logoartmaker4.src} alt="logoartmaker4" style={{marginRight: '15px'}}/>
+			<Divider style={{width: '28px', margin: '0 0 0 0', transform: 'rotate(90deg)'}}/>
+			{/* <figure id="reanimate-header-logo"> */}
+				{/* <img
 					src={LogoReanimate.src}
 					alt="Reanimate and logo"
 					className="reanimate-image-logo"
-				/>
+				/> */}
 				<figcaption className="iconColorOrange dynamic-title">Dynamic Ads</figcaption>
-			</figure>
+			{/* </figure> */}
 
-			<div id="profile-header-icon">
+			{/* <div id="profile-header-icon">
 				<AccountCircleIcon
 					className="iconColorOrange"
 					fontSize="large"
 				/>
-			</div>
+			</div> */}
 		</S.HeaderContainer>
 	);
 };
 
 export default Header;
+
+
+// import HomeIcon from '@mui/icons-material/Home';
+// import LogoutIcon from '@mui/icons-material/Logout';
+// import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+// import * as S from './style';
+// import LogoReanimate from '../../../public/logoreanimatenew.png';
+
+// const Header = () => {
+// 	return (
+// 		<S.HeaderContainer>
+// 			<div id="control-header-buttons">
+// 				<HomeIcon className="iconColorOrange" fontSize="large" />
+// 				<LogoutIcon className="iconColorOrange" fontSize="large" />
+// 			</div>
+
+// 			<figure id="reanimate-header-logo">
+// 				<img
+// 					src={LogoReanimate.src}
+// 					alt="Reanimate and logo"
+// 					className="reanimate-image-logo"
+// 				/>
+// 				<figcaption className="iconColorOrange dynamic-title">Dynamic Ads</figcaption>
+// 			</figure>
+
+// 			<div id="profile-header-icon">
+// 				<AccountCircleIcon
+// 					className="iconColorOrange"
+// 					fontSize="large"
+// 				/>
+// 			</div>
+// 		</S.HeaderContainer>
+// 	);
+// };
+
+// export default Header;
+
+
